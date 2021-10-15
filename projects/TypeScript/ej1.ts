85c3ea9dedad017ed173b617ac18e35583c17a88
Josué Romero <79048209+josueromram@users.noreply.github.com>
2021-10-14 22:57:40 -0500

    Delete projects directory

--- projects/TypeScript/ej1.ts
+++
@@ -1,9 +0,0 @@
-function migreeter(person: string) {
-    return "Hello, " + person;
-}
-
-let miuser = "Jane User";
-
-document.body.innerHTML = migreeter(miuser);
-
-console.log(migreeter(miuser));
